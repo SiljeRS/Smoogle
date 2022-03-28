db609dfa80e115885ff79db7fef17eb271463b6c
MadsHoltum <au683315@post.au.dk>
2022-03-28 14:02:57 +0200

    Oprettede blinds og light

---
+++ Smoogle/blinds.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoogle
+{
+    public class Blinds
+    {
+        private bool blindsAreDown = false;
+        public void blindsDown()
+        {
+            if(blindsAreDown)
+                Console.WriteLine("Blinds are already down");
+            else
+                Console.WriteLine("Blind was rolled down");
+        }
+    }
+}
