c99a02fe89102c416c1e53a4559f909c875be36b
SiljeRS <68823039+SiljeRS@users.noreply.github.com>
2022-03-29 10:06:39 +0200

    Coffee commands Make and Clean added

--- Smoogle/Commands/CommandInterface.cs
+++ Smoogle/Commands/CommandInterface.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Smoogle.Commands
 {
-    interface Command
+    public interface Command
     {
         void Execute();
     }
---
+++ Smoogle/Commands/MakeCoffeeCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smoogle;
+
+namespace Smoogle.Commands
+{
+    public class MakeCoffeeCommand : Command
+    {
+        Coffee _coffee;
+        public MakeCoffeeCommand(Coffee coffee)
+        {
+            _coffee = coffee;
+        }
+        public void Execute()
+        {
+            _coffee.on();
+            _coffee.getBeans();
+            _coffee.brew();
+            _coffee.off();
+        }
+    }
+}
