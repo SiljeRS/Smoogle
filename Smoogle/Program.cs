a1d23021c9896863c3a37bcff793d79d8d886fc2
MadsHoltum <au683315@post.au.dk>
2022-03-29 11:42:05 +0200

    Tilføjet coffeeMachine til programmet

--- Smoogle/Program.cs
+++ Smoogle/Program.cs
@@ -3,23 +3,37 @@ using Smoogle;
 using Smoogle.Commands;
 Console.WriteLine("Hello, World!");
 
-//Laver en controller med 2 af hver knap
-SmoogleController sc = new SmoogleController(2);
+//Laver en controller med 3 af hver knap
+SmoogleController sc = new SmoogleController(3);
 
+//Laver en instans af Light
 Light myLight = new Light();
+//Medgiver myLight i parameteren af on og of command for light
 LightOnCommand lightOnCommand = new LightOnCommand(myLight);
 LightOffCommand lightOffCommand = new LightOffCommand(myLight);
+//Tilknytter de to kommandoer til index 0 på controlleren
 sc.setCommand(0, lightOnCommand, lightOffCommand);
 
+//Laver en instans af Blinds
 Blinds myBlinds = new Blinds();
+//Medgiver myBlinds i parameteren for up og down kommandoerne for Blinds
 BlindsDownCommand blindsDownCommand = new BlindsDownCommand(myBlinds);
 BlindsUpCommand blindsUpCommand = new BlindsUpCommand(myBlinds);
-
+//Tilknytter de to kommandoer til index 1 på controlleren
 sc.setCommand(1, blindsDownCommand, blindsUpCommand);
+
+//Laver en instans af Coffee med 100 beans
+Coffee myCoffee = new Coffee(100);
+//Medgiver myCoffee i parameteren for MakeCoffeeCommand og CleanCoffeeMachineCommand
+MakeCoffeeCommand makeCoffeeCommand = new MakeCoffeeCommand(myCoffee);
+CleanCoffeeMachineCommand cleanCoffeeMachineCommand = new CleanCoffeeMachineCommand(myCoffee);
+//Tilknytter de to kommandoer til index 2 på controlleren
+sc.setCommand(2, makeCoffeeCommand, cleanCoffeeMachineCommand);
 
 string input = "";
 string number = "";
 int intNumber = 0;
+
 while(input != "e")
 {
     Console.WriteLine("");
